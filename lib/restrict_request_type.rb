92132b3a9987c8f1ecffcb63bd49b1b475e17abe
Unni <unni.tallman@gmail.com>
2014-04-25 16:06:50 +0530

    removed ununsed variable 'action'

--- lib/restrict_request_type.rb
+++ lib/restrict_request_type.rb
@@ -8,7 +8,6 @@ module RestrictRequestType
 
       before_filter(only: options[:of]) do |controller|
         format = controller.request.format.to_sym
-        action = controller.params['action'].to_sym
         handle_request_with_invalid_format if options[:to].exclude?(format)
       end
     end
